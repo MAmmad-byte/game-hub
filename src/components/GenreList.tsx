41f7f1e8943826b1714fc76bfd993a36cd0c38b9
M Ammad <muhammadammad146@gmail.com>
2023-04-26 23:36:26 +0500

    Fetch the genres.

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,17 @@
+import useGenres from "../hooks/useGenre";
+
+const GenreList = () => {
+  const { genres, error, isLoading } = useGenres();
+  return (
+    <>
+      {error && <p>{error}</p>}
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GenreList;
