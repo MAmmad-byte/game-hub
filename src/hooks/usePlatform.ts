5deba134f7962d4d842458b44d410d96d9c5ef03
M Ammad <muhammadammad146@gmail.com>
2023-05-02 23:20:35 +0500

    shift platform with the app.

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,11 +1,11 @@
 import useData from "./useData";
-
+import platform from "../data/platform";
 export interface Platform{
     id:number;
     name:string;
     slug:string
 }
 
-const usePlatform = ()=> useData<Platform>("/platforms/lists/parents")
+const usePlatform = ()=> ({data:platform})
 
 export default usePlatform
